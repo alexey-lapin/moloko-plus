80de00e96b6c902d359d88379310a4e78ad8ad5c
Alexey Lapin <alexey-lapin@protonmail.com>
2025-08-16 22:19:24 -0400

    fix pwa background

--- src/components/EventEditor.vue
+++ src/components/EventEditor.vue
@@ -89,135 +89,127 @@ watch(
 
 <template>
   <!--  <p class="text-lg"># {{ event.id }} {{ event.name }}</p>-->
-  <div class="mt-0">
-    <p>Start:</p>
-    <div class="flex flex-wrap gap-2">
-      <DatePicker
-        v-if="isStartedAtEditable"
-        class="w-20"
-        v-model="startedAt"
-        timeOnly
-        @keydown.enter="updateStartedAt(startedAt)"
-      />
-      <Button v-if="isStartedAtEditable" label="OK" @click="updateStartedAt(startedAt)" />
-      <template v-for="n in Array.from({ length: 5 }, (_, i) => -5 + i)" :key="n">
+  <div class="molokoplus-event-editor">
+    <div class="mt-0">
+      <p>Start:</p>
+      <div class="flex flex-wrap gap-2">
+        <DatePicker
+          v-if="isStartedAtEditable"
+          class="w-20"
+          v-model="startedAt"
+          timeOnly
+          @keydown.enter="updateStartedAt(startedAt)"
+        />
+        <Button v-if="isStartedAtEditable" label="OK" @click="updateStartedAt(startedAt)" />
+        <template v-for="n in Array.from({ length: 5 }, (_, i) => -5 + i)" :key="n">
+          <Button
+            v-if="!isStartedAtEditable"
+            :label="'' + n"
+            severity="secondary"
+            @click="updateStartedAt(addMinutes(n))"
+          ></Button>
+        </template>
         <Button
           v-if="!isStartedAtEditable"
-          :label="'' + n"
+          @click="isStartedAtEditable = true"
+          icon="pi pi-pencil"
           severity="secondary"
-          @click="updateStartedAt(addMinutes(n))"
         ></Button>
-      </template>
-      <Button
-        v-if="!isStartedAtEditable"
-        @click="isStartedAtEditable = true"
-        icon="pi pi-pencil"
-        severity="secondary"
-      ></Button>
+      </div>
     </div>
-  </div>
 
-  <div class="mt-2">
-    <p>Tags:</p>
-    <SelectButton
-      v-model="tags"
-      :options="tagOptions"
-      data-key="value"
-      option-label="value"
-      option-value="value"
-      multiple
-      @change="
-        updateEvent(false, {
-          properties: {
-            ...event.properties,
-            brest: tags,
-          },
-        })
-      "
-    >
-      <template #option="slotProps">
-        <span :class="slotProps.option.class">{{ slotProps.option.value }}</span>
-      </template>
-    </SelectButton>
-  </div>
+    <div class="mt-2">
+      <p>Tags:</p>
+      <SelectButton
+        v-model="tags"
+        :options="tagOptions"
+        data-key="value"
+        option-label="value"
+        option-value="value"
+        multiple
+        @change="
+          updateEvent(false, {
+            properties: {
+              ...event.properties,
+              brest: tags,
+            },
+          })
+        "
+      >
+        <template #option="slotProps">
+          <span :class="slotProps.option.class">{{ slotProps.option.value }}</span>
+        </template>
+      </SelectButton>
+    </div>
 
-  <div class="mt-2">
-    <p>End:</p>
-    <div class="flex flex-wrap gap-2">
-      <DatePicker
-        v-if="isEndedAtEditable"
-        class="w-20"
-        v-model="endedAt"
-        timeOnly
-        @keydown.enter="updateEndedAt(endedAt)"
-      />
-      <Button v-if="isEndedAtEditable" label="OK" @click="updateEndedAt(endedAt)" />
-      <template v-for="n in Array.from({ length: 5 }, (_, i) => -5 + i)" :key="n">
+    <div class="mt-2">
+      <p>End:</p>
+      <div class="flex flex-wrap gap-2">
+        <DatePicker
+          v-if="isEndedAtEditable"
+          class="w-20"
+          v-model="endedAt"
+          timeOnly
+          @keydown.enter="updateEndedAt(endedAt)"
+        />
+        <Button v-if="isEndedAtEditable" label="OK" @click="updateEndedAt(endedAt)" />
+        <template v-for="n in Array.from({ length: 5 }, (_, i) => -5 + i)" :key="n">
+          <Button
+            v-if="!isEndedAtEditable"
+            :label="'' + n"
+            severity="secondary"
+            @click="updateEndedAt(addMinutes(n))"
+          ></Button>
+        </template>
+        <Button v-if="!isEndedAtEditable" label="Now" @click="updateEndedAt(addMinutes(0))" />
         <Button
           v-if="!isEndedAtEditable"
-          :label="'' + n"
+          @click="isEndedAtEditable = true"
+          icon="pi pi-pencil"
           severity="secondary"
-          @click="updateEndedAt(addMinutes(n))"
         ></Button>
-      </template>
-      <Button v-if="!isEndedAtEditable" label="Now" @click="updateEndedAt(addMinutes(0))" />
-      <Button
-        v-if="!isEndedAtEditable"
-        @click="isEndedAtEditable = true"
-        icon="pi pi-pencil"
-        severity="secondary"
-      ></Button>
+      </div>
     </div>
-  </div>
 
-  <div class="mt-2 mb-2">
-    <p>Comment:</p>
-    <InputText
-      v-model="comment"
-      @change="
-        updateEvent(false, {
-          properties: {
-            ...event.properties,
-            comment,
-          },
-        })
-      "
-    />
+    <div class="mt-2 mb-2">
+      <p>Comment:</p>
+      <InputText
+        v-model="comment"
+        @change="
+          updateEvent(false, {
+            properties: {
+              ...event.properties,
+              comment,
+            },
+          })
+        "
+      />
+    </div>
   </div>
 </template>
 
-<style scoped>
-:deep(.p-togglebutton.p-togglebutton-checked:has(.left)::before) {
-  background-color: var(--p-primary-100);
-}
-
-:deep(.p-togglebutton.p-togglebutton-checked:has(.left)) {
-  color: var(--p-primary-700);
-}
-
-:deep(.p-togglebutton.p-togglebutton-checked:has(.right)::before) {
-  background-color: var(--p-sky-200);
-}
-
-:deep(.p-togglebutton.p-togglebutton-checked:has(.right)) {
-  color: var(--p-sky-700);
-}
-
-:deep(.p-togglebutton.p-togglebutton-checked:has(.d3)::before) {
-  background-color: var(--p-orange-100);
-}
-
-:deep(.p-togglebutton.p-togglebutton-checked:has(.d3)) {
-  color: var(--p-orange-700);
-}
-
-@media (prefers-color-scheme: dark) {
-  :deep(.p-togglebutton.p-togglebutton-checked:has(.left)::before) {
-    background-color: rgb(216 180 254 / 0.35) !important;
+<style>
+.molokoplus-event-editor {
+  .p-togglebutton-checked .p-togglebutton-content:has(.left) {
+    background-color: var(--p-primary-100);
+    color: var(--p-primary-700);
   }
 
-  :deep(.p-togglebutton.p-togglebutton-checked:has(.left)) {
-    color: var(--p-purple-200);
+  .p-togglebutton-checked .p-togglebutton-content:has(.right) {
+    background-color: var(--p-sky-200);
+    color: var(--p-sky-700);
+  }
+
+  :deep(.p-togglebutton-checked .p-togglebutton-content:has(.d3)) {
+    background-color: var(--p-orange-100);
+    color: var(--p-orange-700);
+  }
+}
+
+.dark .molokoplus-event-editor {
+  .p-togglebutton-checked .p-togglebutton-content:has(.left) {
+    background-color: rgb(216 180 254 / 0.35) !important;
+    color: var(--p-purple-200) !important;
   }
 }
 </style>
